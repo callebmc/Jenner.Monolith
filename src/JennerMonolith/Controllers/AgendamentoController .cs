74b71ee2d60412f09519695a99665ba519c154bc
CallebCecco <calleb.malinoski@gmail.com>
2022-06-21 21:06:07 -0300

    Versão funcional do monolito

---
+++ src/JennerMonolith/Controllers/AgendamentoController .cs
@@ -0,0 +1,44 @@
+﻿using JennerMonolith.Agendamento.API.Services;
+using JennerMonolith.Comum.Models;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JennerMonolith.Controllers
+{
+    [ApiController]
+    [Route("api/agendamento")]
+    public class AgendamentoController : ControllerBase
+    {
+        
+        private readonly ISender sender;
+
+        private CancellationToken Token => HttpContext?.RequestAborted ?? default;
+
+        public AgendamentoController(ISender sender)
+        {
+            this.sender = sender ?? throw new System.ArgumentNullException(nameof(sender));
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> Create([FromBody] AgendamentoCreate novoAgendamento)
+        {
+            Aplicacao result;
+            try
+            {
+                result = await sender.Send(novoAgendamento);
+            }
+            catch (System.Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+
+            return Ok(result);
+
+        }
+    }
+}
---
+++ src/JennerMonolith/Services/Carteira/CarteiraSingle.cs
@@ -0,0 +1,38 @@
+﻿using JennerMonolith.Comum.Models;
+using JennerMonolith.Data;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace JennerMonolith.Services
+{
+    public class CarteiraSingle : IRequest<IEnumerable<Carteira>>
+    {
+        public string Cpf { get; set; }
+        public string NomePessoa { get; set; }
+    }
+
+    public class CarteiraSingleHandler : IRequestHandler<CarteiraSingle, IEnumerable<Comum.Models.Carteira>>
+    {
+        private IHttpContextAccessor HttpContextAccessor { get; }
+        private readonly IMongoDatabase MongoDatabase;
+
+        public CarteiraSingleHandler(IHttpContextAccessor httpContextAccessor, IMongoDatabase mongoDatabase)
+        {
+            HttpContextAccessor = httpContextAccessor ?? throw new ArgumentNullException(nameof(httpContextAccessor));
+            MongoDatabase = mongoDatabase ?? throw new ArgumentNullException(nameof(mongoDatabase));
+        }
+
+        public async Task<IEnumerable<Comum.Models.Carteira>> Handle(CarteiraSingle request, CancellationToken cancellationToken)
+        {
+            IEnumerable<Comum.Models.Carteira> carteiraResult = await MongoDatabase
+                .GetCarteiraCollection().GetOneAsync(request.Cpf, request.NomePessoa);
+
+            return await Task.FromResult(carteiraResult);
+        }
+    }
+}
